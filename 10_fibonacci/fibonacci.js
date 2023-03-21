51a9a8e09aefba4bdd48c75a8eeedd53bb973212
mfrotter <mfrotter@yahoo.com>
2023-03-21 00:07:44 -0700

    add solutions to problems 10,11, and 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+
+    let fibArray = [1,1];
+    let pushArray = 0;
+
+    if(index <= 0){
+        return "OOPS"
+    }
+
+    if(index == 1 || index == 2){
+        return 1;
+    }
+
+    for(let i = 2; i < index; i++){
+        fibArray.push(fibArray[i-2] + fibArray[i-1]);
+    }
+
+    return fibArray[index-1];
 
 };
 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a,b) {
+        let nowDate = new Date();
 
+        if(a.yearOfDeath == undefined){
+            a.yearOfDeath = nowDate.getFullYear();
+            console.log(a.yearOfDeath);
+        }
+
+        if(b.yearOfDeath == undefined){
+            b.yearOfDeath = nowDate.getFullYear();
+            console.log(b.yearOfDeath);
+        }
+
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
